885d7a2033478eb2866ad9fe993ae1b6a29c1d86
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-06-10 21:54:21 +0200

    modificate App.js

--- js/App.js
+++ js/App.js
@@ -1,11 +1,27 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+//VARIABLES TO COMUNICATE WITH SERVER
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api',
+    myHeaders = {
+        'X-Client-Id': 'X-Client-Id',
+  'X-Auth-Token': 'X-Auth-Token'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.createColumn(col);
+    });
 }
 
 // TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
